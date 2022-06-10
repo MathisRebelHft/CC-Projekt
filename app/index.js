8354f8013b1cb0d90a090b98a0b6683dd00b493f
Mathis Rebel <02rema1bif>
2022-06-10 17:26:02 +0200

    mongodb now working

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ const execSync = require('child_process').execSync;
 // runs linux command to find ip
 const ipadress = execSync('hostname -I', { encoding: 'utf-8' });  // the default is 'buffer'
 
-/*/ ---------Connect to MongoDB---------------
+// ---------Connect to MongoDB---------------
 mongoose
   .connect(
     'mongodb://mongo:27017/web',
@@ -40,12 +40,6 @@ app.post('/item/add', (req, res) => {
 
   newItem.save().then(item => res.redirect('/'));
 });
-
-*/
-
-app.get('/', (req, res) =>
-    res.json({ message: `Docker is easy 🐳: a message from ${ipadress}` })
-);
 
 const port = 5000;
 app.listen(port, 'web', () => console.log(`app ${ipadress} listening version:2`));
